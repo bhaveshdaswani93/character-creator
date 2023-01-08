220e866edbe487fee3fc89a88ffa8667f8db2865
bhavesh <bhavesh.daswani@toptal.com>
2023-01-08 11:58:50 +0530

    working on solution for excersice 1, 2, 3

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,6 +10,7 @@ const Footer = () => {
         href="https://www.openpeeps.com/"
         target="_blank"
         rel="noopener noreferrer"
+        className={styles.footer_link}
       >
         Open Peeps
       </a>
